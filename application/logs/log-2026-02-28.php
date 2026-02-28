df43a7952108652f99462e11b8498173a2e19ec7
john wati <john.wati.ke@gmail.com>
2026-02-28 07:23:05 +0300

    update config"

--- application/logs/log-2026-02-28.php
+++ application/logs/log-2026-02-28.php
@@ -2,3 +2,9 @@
 
 ERROR - 2026-02-28 03:59:59 --> Severity: Warning --> session_start(): Failed to read session data: user (path: ) /var/www/html/application/libraries/Session/App_Session.php 77
 ERROR - 2026-02-28 07:00:08 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/system/core/Exceptions.php:271) /var/www/html/system/helpers/url_helper.php 564
+ERROR - 2026-02-28 07:20:55 --> Could not find the language line "phone_settings"
+ERROR - 2026-02-28 07:21:18 --> Could not find the language line "phone_settings"
+ERROR - 2026-02-28 07:21:33 --> Could not find the language line "phone_settings"
+ERROR - 2026-02-28 07:21:48 --> Could not find the language line "phone_settings"
+ERROR - 2026-02-28 07:22:02 --> Could not find the language line "phone_settings"
+ERROR - 2026-02-28 07:22:15 --> Could not find the language line "phone_settings"
